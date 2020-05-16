ac462ddec61b7eb89ddc5f356c37e50239e701cc
Kris Foster <krisfoster@live.ie>
2020-05-16 12:45:32 +0100

    d3 barchart initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# d3-barchart
+
+[D3.js (Data Driven Documents)](https://d3js.org/)
---
+++ index.js
@@ -0,0 +1,57 @@
+const data = [
+  { name: 'John', score: 80 },
+  { name: 'Simon', score: 76 },
+  { name: 'Samantha', score: 90 },
+  { name: 'Patrick', score: 82 },
+  { name: 'Mary', score: 90 },
+  { name: 'Christina', score: 75 },
+  { name: 'Michael', score: 86 },
+];
+
+const width = 900;
+const height = 450;
+const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+
+const svg = d3.select('#d3-container')
+  .append('svg')
+  .attr('width', width - margin.left - margin.right)
+  .attr('height', height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]);
+
+const x = d3.scaleBand()
+  .domain(d3.range(data.length))
+  .range([margin.left, width - margin.right])
+  .padding(0.1)
+
+const y = d3.scaleLinear()
+  .domain([0, 100]).nice()
+  .range([height - margin.bottom, margin.top])
+
+svg
+  .append("g")
+  .attr("fill", 'royalblue')
+  .selectAll("rect")
+  .data(data.sort((a, b) => d3.descending(a.score, b.score)))
+  .join("rect")
+    .attr("x", (d, i) => x(i))
+    .attr("y", d => y(d.score))
+    .attr('title', (d) => d.score)
+    .attr("class", "rect")
+    .attr("height", d => y(0) - y(d.score))
+    .attr("width", x.bandwidth());
+
+function yAxis(g) {
+  g.attr("transform", `translate(${margin.left}, 0)`)
+    .call(d3.axisLeft(y).ticks(null, data.format))
+    .attr("font-size", '20px')
+}
+
+function xAxis(g) {
+  g.attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickFormat(i => data[i].name))
+    .attr("font-size", '20px')
+}
+
+svg.append("g").call(xAxis);
+svg.append("g").call(yAxis);
+svg.node();
